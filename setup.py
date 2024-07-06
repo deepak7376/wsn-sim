a11e37bcbd3b43e2923d741b369e60d0ec638d61
DeepakYadav <41716815+deepak7376@users.noreply.github.com>
2024-07-06 13:40:06 +0000

    added version code in setup

--- setup.py
+++ setup.py
@@ -1,10 +1,36 @@
 # setup.py
 from setuptools import setup, find_packages
+import subprocess
+import os
+
+# Get the version from git tags
+version = (
+    subprocess.run(["git", "describe", "--tags"], stdout=subprocess.PIPE)
+    .stdout.decode("utf-8")
+    .strip()
+)
+
+if "-" in version:
+    v, i, s = version.split("-")
+    version = v + "+" + i + ".git." + s
+
+assert "-" not in version
+assert "." in version
+
+# Write the version to a file
+with open("VERSION", "w", encoding="utf-8") as fh:
+    fh.write("%s\n" % version)
+
+# Read the long description from the README file
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='wsn_sim',
-    version='0.1.0',
+    version=version,
     description='Wireless Sensor Network Simulator with AODV and DSR protocols',
+    long_description=long_description, 
+    long_description_content_type='text/markdown',
     author='Deepak Yadav',
     author_email='dky.united@gmail.com',
     url='https://github.com/deepak7376/wsn_sim',
